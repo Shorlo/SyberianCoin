529b78e400594fef93cfbec133296a5af54d92ef
shorlo <javier@Hermes.local>
2023-03-07 19:02:31 +0100

    create validateChain() method

--- src/app.js
+++ src/app.js
@@ -6,4 +6,10 @@ syberianCoin.setBlock(new Block('07/03/2023', {amount: 10}));
 syberianCoin.setBlock(new Block('18/02/2023', {amount: 40}));
 syberianCoin.setBlock(new Block('20/02/2023', {amount: 100}));
 
+console.log(syberianCoin.validateChain());
+
+syberianCoin.chain[1].data = { amount: 3000 };
+syberianCoin.chain[1].hash = syberianCoin.chain[1].calculateHash();
+
+console.log(syberianCoin.validateChain());
 console.log(JSON.stringify(syberianCoin, null, 4));
--- src/blockchain.js
+++ src/blockchain.js
@@ -22,6 +22,26 @@ class BlockChain
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    validateChain()
+    {
+        for(let i = 1; i < this.chain.length; i++)
+        {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash != currentBlock.calculateHash())
+            {
+                return false;
+            }
+
+            if(currentBlock.previousHash != previousBlock.hash)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 module.exports = BlockChain;
