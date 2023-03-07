d53186841d9008be0baab037d21a80c32ce8c232
shorlo <javier@Hermes.local>
2023-03-07 18:37:05 +0100

    creating new blocks

--- src/app.js
+++ src/app.js
@@ -38,3 +38,11 @@ class BlockChain
         this.chain.push(newBlock);
     }
 }
+
+
+let syberianCoin = new BlockChain();
+syberianCoin.setBlock(new Block('07/03/2023', {amount: 10}));
+syberianCoin.setBlock(new Block('18/02/2023', {amount: 40}));
+syberianCoin.setBlock(new Block('20/02/2023', {amount: 100}));
+
+console.log(JSON.stringify(syberianCoin, null, 4));
