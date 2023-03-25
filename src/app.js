1d91f570e49445520a8f326612de392cdb54429d
Shorlo <shorlo.sbg@gmail.com>
2023-03-25 13:14:30 +0100

    added header

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+/*   SyberianCoin/app.js
+       ____     __           _           _____        __
+      / __/_ __/ /  ___ ____(_)__  ___  / ___/__  ___/ /__
+ --- _\ \/ // / _ \/ -_) __/ / _ `/ _ \/ /__/ _ \/ _  / -_)---------------------
+|   /___/\_, /_.__/\__/_/ /_/\_,_/_//_/\___/\___/\_,_/\__/                      |
+|       /___/                                                                   |
+|                                                                               |
+|   Copyright © 2022-2023 Javier Sainz de Baranda Goñi.                         |
+|   Released under the terms of the GNU Lesser General Public License v3.       |
+|                                                                               |
+|   This program is free software: you can redistribute it and/or modify it     |
+|   under the terms of the GNU General Public License as published by the Free  |
+|   Software Foundation, either version 3 of the License, or (at your option)   |
+|   any later version.                                                          |
+|  This program is distributed in the hope that it will be useful, but          |
+|  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   |
+|  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License      |
+|  for more details.                                                            |
+|                                                                               |
+|  You should have received a copy of the GNU General Public License along      |
+|  with this program. If not, see <http://www.gnu.org/licenses/>.               |
+|                                                                               |
+'==============================================================================*/
+
 const Block = require('./block');
 const BlockChain = require('./blockchain');
 const Transaction = require('./transaction');
